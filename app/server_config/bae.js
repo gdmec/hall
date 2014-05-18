2bd952b5d2359999d88ddea10576571fdf702078
York Cui <cuiyu2000@sina.com>
2014-05-19 01:46:25 +0800

    modify bae.js

--- app/server_config/bae.js
+++ app/server_config/bae.js
@@ -1,9 +1,9 @@
 //config file for bae
 if(sumeru.BAE_VERSION){
   sumeru.config.database({
-    dbname : 'yourdbname',
-    user: 'yourpk',//bae 3.0 required
-    password: 'yoursk',//bae 3.0 required
+    dbname : 'NALcEuVVqrDieXyuNlzo',
+    user: 'SzPGMhRbcvryutv56Cy43f12',//bae 3.0 required
+    password: 'ftyM4LymL5erbZMqAOEvK2myHqMixfTE',//bae 3.0 required
   }); 
   sumeru.config({
     site_url : '', //with tailing slash
